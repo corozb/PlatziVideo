e310f88444c0fe96a0175f8fd0ef1c79bd558c44
Cristian Orozco <cristianorozco1984@gmail.com>
2020-01-03 16:00:37 -0500

    ESLint

---
+++ src/components/JelouWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const JellouWorld = () => (
+  <h1>Hola Pues</h1>
+)
+
+export default JellouWorld
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import JelouWorld from './components/JelouWorld';
+
+ReactDOM.render(<JelouWorld />, document.getElementById('app'));
