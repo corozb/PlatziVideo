3d3064366a63b08459594960bccf8a39fb17dcdd
Cristian Orozco <cristianorozco1984@gmail.com>
2020-02-19 16:06:01 -0500

    register

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginRequest } from '../actions';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/App.scss';
 
-const Login = () => {
+const Login = props => {
   const [form, setValues] = useState({
     email: ''
   });
@@ -18,7 +20,8 @@ const Login = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(form);
+    props.loginRequest(form);
+    props.history.push('/');
   };
 
   return (
@@ -66,4 +69,8 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/App.scss';
 
-const Register = () => (
-  <section className='register'>
-    <section className='register__container'>
-      <h2>Regístrate</h2>
-      <form className='register__container-form'>
-        <input className='input' type='text' placeholder='Nombre' />
-        <input className='input' type='text' placeholder='Correo' />
-        <input className='input' type='password' placeholder='Contraseña' />
-        <button className='button'>Registrarme</button>
-      </form>
-      <Link to='/login'>Iniciar sesión</Link>
+const Register = () => {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: ''
+  });
+
+  const handleInput = e => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <section className='register'>
+      <section className='register__container'>
+        <h2>Regístrate</h2>
+        <form className='register__container-form' onSubmit={handleSubmit}>
+          <input
+            name='name'
+            className='input'
+            type='text'
+            placeholder='Nombre'
+            onChange={handleInput}
+          />
+          <input
+            name='email'
+            className='input'
+            type='text'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button className='button'>Registrarme</button>
+        </form>
+        <Link to='/login'>Iniciar sesión</Link>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default Register;
